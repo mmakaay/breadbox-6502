553b90ba4740deaecb518ca4a87b26973bd69c1d
Maurice Makaay <maurice@h2b.nl>
2026-02-15 00:09:52 +0100

    Expand test-UM6551 with status bit display
    Show ACIA status register bits on LCD line 2 for hardware debugging.
    Read data register directly on RXFULL and display received char.
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-opencode)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- projects/test-UM6551/test-um6551.s
+++ projects/test-UM6551/test-um6551.s
@@ -4,32 +4,74 @@
 
     hello: .asciiz "Serial test"
 
+.segment "ZEROPAGE"
+
+    status_val: .res 1
+
 .segment "CODE"
 
     main:
         jsr LCD::clr
-        jsr @welcome
+        jsr welcome
 
-    @wait_for_rx:
-        jsr SERIAL::check_rx
-        beq @wait_for_rx
+    @poll:
+        ; Read ACIA status register directly
+        lda $5001
+        sta status_val
 
-        jsr SERIAL::read
-        jsr LCD::send_data
-        jmp @wait_for_rx
+        ; Show status bits on LCD line 2
+        jsr show_status
 
-    @welcome:
+        ; Check RXFULL (bit 3)
+        lda status_val
+        and #$08
+        beq @poll
+
+        ; Data received - read it and show on LCD line 1
+        lda $5000           ; Read ACIA DATA register
         pha
-        phx
+        jsr LCD::home
+        pla
+        jsr LCD::write
+
+        jmp @poll
+
+
+    .proc show_status
+        ; Position cursor at start of LCD line 2
+        lda #$c0            ; Set DDRAM address = $40 (line 2)
+        jsr LCD::write_instruction
+
+        ; Display "S:" prefix
+        lda #'S'
+        jsr LCD::write
+        lda #':'
+        jsr LCD::write
+
+        ; Display 8 status bits, MSB first
+        ; Bit meaning: IRQ DSR DCD TXE RXF OVR FRM PAR
+        ldx #8
+    @loop:
+        asl status_val      ; Shift MSB into carry
+        lda #'0'
+        adc #0              ; '0' + carry = '0' or '1'
+        jsr LCD::write
+        dex
+        bne @loop
+
+        rts
+    .endproc
+
+
+    .proc welcome
         ldx #0
     @loop:
         lda hello,x
         beq @done
-        jsr LCD::send_data
+        jsr LCD::write
         inx
-        bra @loop
+        jmp @loop
     @done:
-        plx
-        pla
         rts
+    .endproc
 
